e67013ca2947e2cfb1c60aa8e474382691547f4a
Sergey Emelyanov <karagabul@gmail.com>
2020-08-23 07:36:06 +0300

    Add code 07

---
+++ code_07.py
@@ -0,0 +1,31 @@
+def cache(func):
+    _cache = {}
+    def inner(*args):
+        result = _cache.get(args)
+        if result is not None:
+            return result
+        else:
+            result = func(*args)
+            _cache[args] = result
+            return result
+    return inner
+
+
+@cache
+def my_sum(a, b):
+    print('Call my_sum')
+    return a + b
+
+
+@cache
+def my_diff(a, b):
+    print('Call my_diff')
+    return a - b
+
+
+print(my_sum(1, 2))
+print(my_sum(1, 2))
+print(my_diff(1, 2))
+print(my_diff(1, 2))
+
+
