f0460dcc2a8e191d6e2bac5e730550d09f8dfbb5
Sergey Emelyanov <karagabul@gmail.com>
2020-08-23 08:53:42 +0300

    Add task 05

---
+++ task_05.py
@@ -0,0 +1,22 @@
+"""
+>>> print(my_sum_1(1, 2))
+6
+>>> print(my_sum_2(3, 4))
+21
+"""
+
+# Your code here
+
+
+@decorator(2)
+def my_sum_1(*args):
+    return sum(args)
+
+
+@decorator(3)
+def my_sum_1(*args):
+    return sum(args)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
